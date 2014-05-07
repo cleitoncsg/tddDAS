eaa56069feb54c5b48f6bd7e5d133cec71f7178b
Cleiton Gomes <cleitoncsg@gmail.com>
2014-05-07 14:45:13 -0300

    Passo 1 resolução exercicio StringQuery

---
+++ exerciciosDaAulaTDD/src/jamesshore/com/QueryString.java
@@ -0,0 +1,15 @@
+package jamesshore.com;
+
+public class QueryString {
+
+	private String _query;
+	
+	public QueryString(String queryString) {
+		_query = queryString;
+	}
+
+	public int count() {
+		return 0;
+	}
+
+}
