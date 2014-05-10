ac993f1c914b4fb2495e8ab856b6f715ce1aa78a
thiagohonorato <thiagohonorato93@gmail.com>
2014-05-10 14:37:44 -0300

    Fazendo o teste 3 passar e refatorando Classe Real

--- thiago/ConversorRomano/src/ConversordeRomanos.java
+++ thiago/ConversorRomano/src/ConversordeRomanos.java
@@ -1,14 +1,21 @@
+import java.util.HashMap;
+
 
 public class ConversordeRomanos {
+	
+	private static HashMap<String, Integer> tabela = new HashMap<String,Integer>(){{
+		put("I",1);
+		put("V",5);
+		put("X",10);
+		put("L",50);
+		put("C",100);
+		put("D",500);
+		put("M",1000);
+	}};
 
 	public int converte(String algarismo) {
-		if (algarismo == "I"){
-			return 1;
-		}
-		else if (algarismo == "V"){
-			return 5;
-		}
-		return 0;
+		
+		return tabela.get(algarismo);
 	}
 
 }
