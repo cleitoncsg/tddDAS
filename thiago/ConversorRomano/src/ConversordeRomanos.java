54ca9ab4456eb1e908629b8a4c29375fb5f7ccc4
thiagohonorato <thiagohonorato93@gmail.com>
2014-05-10 15:09:13 -0300

    Refatorando teste 4

--- thiago/ConversorRomano/src/ConversordeRomanos.java
+++ thiago/ConversorRomano/src/ConversordeRomanos.java
@@ -3,20 +3,27 @@ import java.util.HashMap;
 
 public class ConversordeRomanos {
 	
-	private static HashMap<String, Integer> tabela = new HashMap<String,Integer>(){{
-		put("I",1);
-		put("II", 2);
-		put("V",5);
-		put("X",10);
-		put("L",50);
-		put("C",100);
-		put("D",500);
-		put("M",1000);
+	private static HashMap<Character, Integer> tabela = new HashMap<Character,Integer>(){{
+		put('I',1);
+		put('V',5);
+		put('X',10);
+		put('L',50);
+		put('C',100);
+		put('D',500);
+		put('M',1000);
 	}};
 
 	public int converte(String algarismo) {
 		
-		return tabela.get(algarismo);
+		int acumulador = 0;
+		
+		for (int i =0; i < algarismo.length(); i++){
+					
+		acumulador = acumulador + tabela.get(algarismo.charAt(i));
+					
+		}
+
+		return acumulador ;
 	}
 
 }
