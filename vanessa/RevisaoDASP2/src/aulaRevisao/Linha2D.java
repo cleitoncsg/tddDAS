b13174fefc5f423a834e1a625233cb1bba54cd03
Vanessa Barbosa <vanessabmdf@gmail.com>
2014-05-13 16:56:41 -0300

    Green Bar for testInterseccao

---
+++ vanessa/RevisaoDASP2/src/aulaRevisao/Linha2D.java
@@ -0,0 +1,17 @@
+package aulaRevisao;
+
+public class Linha2D {
+	
+	private Ponto pontoA, pontoB;
+
+	public Linha2D(Ponto pontoA, Ponto pontoB) {
+		this.pontoA=pontoA;
+		this.pontoB=pontoB;
+	}
+
+	public String interseccao(Linha2D linhaB) {
+		// TODO Auto-generated method stub
+		return "encontrado";
+	}
+
+}
---
+++ vanessa/RevisaoDASP2/src/aulaRevisao/Ponto.java
@@ -0,0 +1,13 @@
+package aulaRevisao;
+
+public class Ponto {
+	
+	private int x;
+	private int y;
+
+	public Ponto(int pontoX, int pontoY) {
+		this.x=pontoX;
+		this.y =pontoY;
+	}
+
+}
