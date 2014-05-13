2cdf9d8a16252aed696ce2a95b48b87940f27a68
Cleiton Gomes <cleitoncsg@gmail.com>
2014-05-12 23:22:49 -0300

    Teste verde QueryString

---
+++ cleiton/exercicioJames/src/jamesshore/com/QueryString.java
@@ -0,0 +1,16 @@
+package jamesshore.com;
+
+public class QueryString {
+
+	private String name_query;
+	
+	public QueryString(String _query) {
+		name_query = _query;
+	}
+
+	public long count() {
+		return 1;
+	}
+
+
+}
