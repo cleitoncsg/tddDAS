dba7078468e7cffb43fe901840bf3ba29d42982d
Cleiton Gomes <cleitoncsg@gmail.com>
2014-05-19 19:54:52 -0300

    Refactoring for test verificaSeEhRetangulo

--- cleiton/exercicioRetangulo/src/retangulo/Retangulo.java
+++ cleiton/exercicioRetangulo/src/retangulo/Retangulo.java
@@ -2,49 +2,12 @@ package retangulo;
 
 public class Retangulo {
 
-	private double xmin,ymin,xmax,ymax;
 
-	
-	
-	
-	
-	
-	
-	
-	public double getXmin() {
-		return xmin;
-	}
-
-	public void setXmin(double xmin) {
-		this.xmin = xmin;
-	}
-
-	public double getYmin() {
-		return ymin;
-	}
-
-	public void setYmin(double ymin) {
-		this.ymin = ymin;
-	}
-
-	public double getXmax() {
-		return xmax;
-	}
-
-	public void setXmax(double xmax) {
-		this.xmax = xmax;
-	}
-
-	public double getYmax() {
-		return ymax;
-	}
-
-	public void setYmax(double ymax) {
-		this.ymax = ymax;
-	}
-
-	public boolean verificaSeEhRetangulo() {
-		return true;
+	public boolean verificaSeEhRetangulo(double xmin,double ymin,double xmax,double ymax) {
+		
+		if( (ymax - ymin) > 0 && (xmax - xmin) > 0)
+			return true;
+		return false;
 	}
 	
 
