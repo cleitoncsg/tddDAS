0b2f8bd2457828c799018e87ade0db5ab2be6bd8
Cleiton Gomes <cleitoncsg@gmail.com>
2014-05-19 19:12:17 -0300

    Red bar for test constroiRetanguloTest

---
+++ cleiton/exercicioRetangulo/src/retangulo/RetanguloTest.java
@@ -0,0 +1,27 @@
+package retangulo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class RetanguloTest {
+
+	Retangulo retangulo;
+	
+	@Before
+	public void setUp() throws Exception {
+		retangulo = new Retangulo();
+	}
+
+	@Test
+	public void constroiRentagulotest() {
+		retangulo.setXmin(1);
+		retangulo.setYmin(1);
+		retangulo.setXmax(2);
+		retangulo.setYmax(2);
+		
+		assertTrue(verificaSeEhRetangulo());
+	}
+
+}
